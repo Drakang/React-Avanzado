3e8ef7ea895ee274959f2870b6cee28a249181dc
David Monroy <david2310.dmc@gmail.com>
2023-01-16 00:44:00 +0000

    AdvertsPage add error code

--- src/components/adverts/AdvertsPage/AdvertsPage.js
+++ src/components/adverts/AdvertsPage/AdvertsPage.js
@@ -1,37 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import FiltersForm from './FiltersForm';
-import AdvertsList from './AdvertsList';
-import EmptyList from './EmptyList';
-import storage from '../../../utils/storage';
-import { getStateAdverts, getUi } from '../../../store/selectors';
-import { defaultFilters, filterAdverts } from './filters';
-import { useDispatch, useSelector } from 'react-redux';
-import { advertsLoad } from '../../../store/actions';
+import FiltersForm from "./FiltersForm";
+import AdvertsList from "./AdvertsList";
+import EmptyList from "./EmptyList";
+import storage from "../../../utils/storage";
+import { getStateAdverts, getUi } from "../../../store/selectors";
+import { defaultFilters, filterAdverts } from "./filters";
+import { useDispatch, useSelector } from "react-redux";
+import { advertsLoad } from "../../../store/actions";
+import { useNavigate } from "react-router-dom";
 
-const getFilters = () => storage.get('filters') || defaultFilters;
-const saveFilters = filters => storage.set('filters', filters);
+const getFilters = () => storage.get("filters") || defaultFilters;
+const saveFilters = (filters) => storage.set("filters", filters);
 
 function AdvertsPage() {
   const [filters, setFilters] = useState(getFilters);
   const { isLoading } = useSelector(getUi);
-  const dispatch = useDispatch()
-  
-  useEffect(()=>{
-    dispatch(advertsLoad())
-    
-  },[dispatch])
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(advertsLoad()).catch((error) => {
+      if (error.statusCode === 404) {
+        navigate("404");
+      }
+    });
+  }, [dispatch, navigate]);
 
   useEffect(() => {
     saveFilters(filters);
   }, [filters]);
 
-  const adverts = useSelector((state)=>getStateAdverts(state).reverse())
+  const adverts = useSelector((state) => getStateAdverts(state).reverse());
 
   const filteredAdverts = filterAdverts(adverts, filters);
 
   if (isLoading) {
-    return 'Loading...';
+    return "Loading...";
   }
 
   return (
